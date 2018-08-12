a492a0da1981c9bf8ac20d9235d05a654b3c41e3
Jacob Americano <jacob_a@live.ca>
2018-08-12 11:14:56 -0400

    Game Repo Setup
    Delayed.
    Game has been setup with the following:
    basic rocket with controls and audio,
    basic launch platform,
    basic terrain.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessUpdate();
+    }
+
+    void ProcessUpdate()
+    {
+        handleShipControl();
+    }
+
+    void handleShipControl()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            enableBoosterAudio();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    void enableBoosterAudio()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
